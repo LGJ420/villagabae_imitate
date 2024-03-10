fadba5fd2054f1e8f74dff1feca57428bd5f7a73
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-03-10 17:53:15 +0900

    모달창 제작완료

--- lgj_server/js/modal.js
+++ lgj_server/js/modal.js
@@ -1,11 +1,23 @@
-const modalOpenButton = document.getElementsByClassName('modal2_btn');
-const modalCloseButton = document.getElementsByClassName('modalCloseButton');
-const modal = document.getElementsByClassName('modal_box2');
+const modal1OpenButton = document.querySelector('.modal1_open');
+const modal1CloseButton = document.querySelector('.modal1_close');
+const modal1 = document.querySelector('.modal1');
 
-modalOpenButton.addEventListener('click', () => {
-    modal.classList.remove('hidden');
-  });
-  
-  modalCloseButton.addEventListener('click', () => {
-    modal.classList.add('hidden');
-  });
+modal1OpenButton.addEventListener("click", ()=>{
+    modal1.style.display = "block"
+})
+
+modal1CloseButton.addEventListener("click", ()=>{
+    modal1.style.display = "none"
+})
+
+const modal2OpenButton = document.querySelector('.modal2_open');
+const modal2CloseButton = document.querySelector('.modal2_close');
+const modal2 = document.querySelector('.modal2');
+
+modal2OpenButton.addEventListener("click", ()=>{
+    modal2.style.display = "block"
+})
+
+modal2CloseButton.addEventListener("click", ()=>{
+    modal2.style.display = "none"
+})
